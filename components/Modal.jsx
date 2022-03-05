9b12073d6aa053315cb29be3cdddb33e2baa05fe
gumifu <ffumi.66@gmail.com>
2022-03-05 14:53:02 +0900

    added modal 2

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,43 +1,132 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
 import { Dialog, Transition } from "@headlessui/react";
+import { CameraIcon } from '@heroicons/react/solid';
 
 const Modal = () => {
     const [open, setOpen] = useRecoilState(modalState);
+    const filePickerRef = useRef(null);
+    const capthionRef = useRef(null);
+    const [loading, setLoading] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(null);
 
-  return (
+    const uploadPost = async () => {
+
+    }
+
+    const addImageToPost = (e) => {
+        const reader = new FileReader();
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+
+        reader.onload = (readerEvent) => {
+            setSelectedFile(readerEvent.target.result);
+        }
+        console.log(reader);
+    }
+
+    return (
+      //   ヘッダーレスUI
       <Transition.Root show={open} as={Fragment}>
-          <Dialog as='div' className='fixed z-10 inset-0 overflow-auto' onClose={setOpen}>
-              <div className='flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 pb-20 text-center sm:block sm:p-0'>
-                  <Transition.Child as={Fragment} enter='ease-out duration-30' enterFrom='opacity-100' leave='ease-in duration-200' leaveFrom='opacity-100' leaveTo='opacity-0'>
+        <Dialog
+          as="div"
+          className="fixed z-10 inset-0 overflow-auto"
+          onClose={setOpen}
+        >
+          <div className="flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-30"
+              enterFrom="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
 
-                      <Dialog.Overlay className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'/>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-100 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:scale-95"
+            >
+              {/* Modalの中身 */}
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6 ">
+                <div>
+                  {selectedFile ? (
+                    <img src={selectedFile} onClick={()=>setSelectedFile(null)} alt="" className='w-full object-contain cursor-pointer' />
+                  ) : (
+                    <div
+                      onClick={() => filePickerRef.current.click()}
+                      className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                    >
+                      <CameraIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  )}
 
-                     </Transition.Child>
-                    <span className='hidden sm:inline-block sm:align-middle sm:h-screen' aria-hidden="true">
-                        &#8203;
-                    </span>
-
-                    <Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-100 translate-y-4 sm:translate-y-0 sm:scale-95' enterTo='opacity-100 translate-y-0 sm:scale-100' leave='ease-in duration-200' leaveFrom='opacity-100 translate-y-0 sm:scale-100' leaveTo='opacity-0 translate-y-4 sm:scale-95'>
-                  <div className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6 '>
-
+                  <div>
+                    <div className="mt-3 text-center sm:mt-5">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg leading-6 font-medium text-gray-900"
+                      >
+                        写真追加
+                      </Dialog.Title>
+                      <div>
+                        <input
+                          ref={filePickerRef}
+                          type="file"
+                          hidden
+                          onChange={addImageToPost}
+                        />
+                      </div>
+                      <div className="mt-2">
+                        <input
+                          className="border-none focus:right-0 w-full text-center"
+                          ref={capthionRef}
+                          type="text"
+                          placeholder="キャプションを追加"
+                        />
+                      </div>
+                    </div>
                   </div>
-                  </Transition.Child>
 
+                  <div className="mt-5 sm:mt-6">
+                    {/* somewhere wrong? */}
+                    <button
+                      type="button"
+                      className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed  hover:disabled:bg-gray-300"
+                    >
+                      アップロード
+                    </button>
+                  </div>
+                </div>
               </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
 
-
-          </Dialog>
-
-
-          {/* {open && (
+        {/* {open && (
 
               <p>The Modal is Open</p>
           )} */}
       </Transition.Root>
-
-  )
+    )
 }
 
 export default Modal
